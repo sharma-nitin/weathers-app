8d1fe07e1b6abccee074889046f4334187c2f1de
Nitin Sharma <nitin.g.sharma@capgemini.com>
2020-12-19 19:52:37 +0530

    weathers app commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,20 +1,14 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { ComponentFixture, inject, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from "./app.component";
-import { WeatherService } from './service/weather.service';
 
 describe('AppComponent', () => {
   let component: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
   beforeEach(() => {
-    const weatherServiceStub = {
-      getWeatherForecast: () => ({ subscribe: f => f({}) })
-
-    };
     TestBed.configureTestingModule({
       schemas: [NO_ERRORS_SCHEMA],
-      declarations: [AppComponent],
-      providers: [{ provide: WeatherService, useValue: weatherServiceStub }]
+      declarations: [AppComponent]
     });
     fixture = TestBed.createComponent(AppComponent);
     component = fixture.componentInstance;
@@ -22,19 +16,12 @@ describe('AppComponent', () => {
   it('can load instance', () => {
     expect(component).toBeTruthy();
   });
-  it('makes call to ngOnInit ', inject([WeatherService],(weatherService) => {
+  it('makes call to ngOnInit ',() => {
     spyOn(component, 'ngOnInit').and.callThrough();
     fixture.whenStable().then(() => {
       component.ngOnInit();
-      weatherService.getWeatherForecast('London').subscribe(result => expect(result).toBeDefined());
       expect(component.ngOnInit()).toHaveBeenCalled();
     });
-  }));
-  it('makes call to findDay ',() => {
-    spyOn(component, 'findDay').and.callThrough();
-    fixture.whenStable().then(() => {
-      component.findDay(2);
-      expect(component.findDay).toHaveBeenCalled();
-    });
   });
+
 })
